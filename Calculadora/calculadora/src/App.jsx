089bdac501f5f72e2c4f1f144855bf2a00b8884e
Taynad <166086044+Taynad@users.noreply.github.com>
2025-10-22 16:12:50 -0300

    calculadora

---
+++ Calculadora/calculadora/src/App.jsx
@@ -0,0 +1,13 @@
+import {Container, Content} from './styles';
+
+function App() {
+  return (
+   <Container>
+    <Content>
+      Olá
+    </Content>
+   </Container>
+  )
+}
+
+export default App
